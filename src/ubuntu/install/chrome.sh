973e567e12cdf812ab0dd1a29a9c2630eb96afed
cuiyiny <cuiyinyin@outlook.com>
2017-12-15 17:57:56 +0800

    修改初始参数

--- src/ubuntu/install/chrome.sh
+++ src/ubuntu/install/chrome.sh
@@ -4,8 +4,9 @@ set -e
 
 echo "Install Chromium Browser"
 apt-get update 
-apt-get install -y chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg
+apt-get install -y chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg \
+	ttf-ubuntu-font-family fonts-wqy-microhei language-pack-zh-hant language-pack-gnome-zh-hant
 apt-get clean -y
-ln -s /usr/bin/chromium-browser /usr/bin/google-chrome
+#ln -s /usr/bin/chromium-browser /usr/bin/google-chrome
 ### fix to start chromium in a Docker container, see https://github.com/ConSol/docker-headless-vnc-container/issues/2
-echo "CHROMIUM_FLAGS='--no-sandbox --start-maximized --user-data-dir'" > $HOME/.chromium-browser.init
+echo "CHROMIUM_FLAGS='--start-maximized --user-data-dir'" > $HOME/.chromium-browser.init
