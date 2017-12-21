5a74cba4c2be80a55dd752de67d446acd665eff5
cuiyiny <cuiyinyin@outlook.com>
2017-12-21 17:56:33 +0800

    用chrome替代chromium

--- src/ubuntu/install/tools.sh
+++ src/ubuntu/install/tools.sh
@@ -4,7 +4,8 @@ set -e
 
 echo "Install some common tools for further installation"
 apt-get update 
-apt-get install -y ssh vim wget net-tools locales bzip2 \
+apt-get install -y vim wget net-tools locales bzip2 \
+	ttf-ubuntu-font-family fonts-wqy-microhei language-pack-zh-hant language-pack-gnome-zh-hant \
     python-numpy #used for websockify/novnc
 apt-get clean -y
 
