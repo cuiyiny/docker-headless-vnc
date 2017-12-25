2b8288f0c35b253a76363f9b500f03c53cf17227
cuiyiny <cuiyinyin@outlook.com>
2017-12-25 15:08:42 +0800

    换一种方式安装chrome

--- src/ubuntu/install/chrome.sh
+++ src/ubuntu/install/chrome.sh
@@ -3,9 +3,7 @@
 set -e
 
 echo "Install Chromium Browser"
-echo "Install Chromium Browser"
-apt-get update 
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
 sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 apt-get update
 apt-get install google-chrome
