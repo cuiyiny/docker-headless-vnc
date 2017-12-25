a8ae5d0203c6dee91a956f27055f89787bf14929
cuiyiny <cuiyinyin@outlook.com>
2017-12-25 14:55:58 +0800

    换一种方式安装chrome

--- src/ubuntu/install/chrome.sh
+++ src/ubuntu/install/chrome.sh
@@ -3,10 +3,9 @@
 set -e
 
 echo "Install Chromium Browser"
+echo "Install Chromium Browser"
 apt-get update 
-apt-get install -y chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg \
-	ttf-ubuntu-font-family fonts-wqy-microhei language-pack-zh-hant language-pack-gnome-zh-hant
-apt-get clean -y
-#ln -s /usr/bin/chromium-browser /usr/bin/google-chrome
-### fix to start chromium in a Docker container, see https://github.com/ConSol/docker-headless-vnc-container/issues/2
-echo "CHROMIUM_FLAGS='--start-maximized --user-data-dir'" > $HOME/.chromium-browser.init
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add
+sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+apt-get update
+apt-get install google-chrome
